96c8db0e6183d61fca7b23f2d98dac2209374491
Russell Samora <russellsamora@gmail.com>
2024-12-01 13:07:55 -0500

    update github pages

--- google.config.js
+++ google.config.js
@@ -6,5 +6,25 @@ export default [
 	{
 		id: "1-0A3Ct2v7gcGkXk0CefprHCJYh_D69KpfQwaivs2eaI",
 		filepath: "static/assets/itineraries/punakha-bhutan.json"
+	},
+	{
+		id: "1Op2hs94Ts2hxxj-SO6dEZ-PgKxkdMh6KZmAtW_pVFdY",
+		filepath: "static/assets/itineraries/spello-italy.json"
+	},
+	{
+		id: "1fi9r--iy4PDN2rBiw9zfc9zWzdCNqcxGoHn3xb75ucc",
+		filepath: "static/assets/itineraries/havana-cuba.json"
+	},
+	{
+		id: "1M2N_IYu5uB1ZhK_ABlagvzp0Bzbv0upmKq1BgFAkSxM",
+		filepath: "static/assets/itineraries/winay_wayna-peru.json"
+	},
+	{
+		id: "1bQEsrGw7M_r1_rpNIsUtukoqMA2Eb071VEGAfZ7sFg4",
+		filepath: "static/assets/itineraries/kyoto-japan.json"
+	},
+	{
+		id: "1HlIa2rY0e-ptj2SWm4xcU953IvvNzfp5-CNPd12QSAk",
+		filepath: "static/assets/itineraries/mekong_delta-vietnam.json"
 	}
 ];
